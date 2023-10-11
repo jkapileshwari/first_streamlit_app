b996adc7107edf04fdcf6dc4dcc0e01efac6cb09
jkapileshwari <147603062+jkapileshwari@users.noreply.github.com>
2023-10-11 15:21:59 +0530

    Update streamlit_app.py
    multiselect option

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,3 +15,11 @@ import pandas
 
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+
+
+#lets put a picklist
+streamlit.multiselect("pick some fruits:",list(my_fruit_list.index))
+
+
+#lets display
+streamlit.dataframe(my_fruit_list)
